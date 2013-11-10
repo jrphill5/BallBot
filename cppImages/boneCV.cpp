ae81afadc77482db994928d338dc4c51b407961a
Stephen <deathicidelag@gmail.com>
2013-11-10 15:46:10 -0500

    added cpp files to interface with camera

---
+++ cppImages/boneCV.cpp
@@ -0,0 +1,73 @@
+/* boneCV.cpp
+ *
+ * Copyright Derek Molloy, School of Electronic Engineering, Dublin City University
+ * www.derekmolloy.ie
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted
+ * provided that source code redistributions retain this notice.
+ *
+ * This software is provided AS IS and it comes with no warranties of any type. 
+ */
+
+#include<iostream>
+#include<opencv2/opencv.hpp>
+#include <cv.h>
+#include <highgui.h>
+#include <math.h>
+using namespace std;
+using namespace cv;
+
+int main()
+{
+    
+    VideoCapture capture(0);
+    capture.set(CV_CAP_PROP_FRAME_WIDTH,1280);
+    capture.set(CV_CAP_PROP_FRAME_HEIGHT,720);
+    if(!capture.isOpened()){
+	    cout << "Failed to connect to the camera." << endl;
+    }
+    Mat frame, edges, hsv(Size(1280,720),CV_8UC3), circlesOut;
+    capture >> frame;
+    if(frame.empty()){
+		cout << "Failed to capture an image" << endl;
+		return -1;
+    }
+    circlesOut = frame;
+    cvtColor(frame, hsv, CV_BGR2HSV);
+    
+    inRange(hsv, (7,160,200), (25,255,255), hsv);
+    //cvtColor(hsv, hsv, CV_HSV2BGR);
+    //cvtColor(hsv, hsv, CV_BGR2GRAY);
+    blur(hsv,hsv,Size(5,5));
+    
+    vector<Vec3f> circles;
+
+    //HoughCircles(src, outarray,#method,dp??,minDist,param1,param2,minRadius,maxRadius)
+    HoughCircles(hsv, circles, CV_HOUGH_GRADIENT, 2, 100, 18, 80, 10, 100);
+
+    for(size_t i = 0; i < circles.size(); i++){
+	    Point center(cvRound(circles[i][0]), cvRound(circles[i][1]));
+	    int radius = cvRound(circles[i][2]);
+
+	    circle(circlesOut, center, 3, Scalar(0,255,0), -1, 8, 0);
+	    circle(circlesOut, center, radius, Scalar(0,255,0), 3, 8, 0);
+
+	    cout << "i found a circle" << endl;
+    }
+
+    /* previous code to test for lines
+     * *************************
+    //inRange(src,lower bound,upper bound,dst)
+    inRange(edges, 25, 160, hsv);
+    //blur(src, output, kernel)
+    blur(edges,edges,Size(5,5));
+    //Canny(src, output, thresh1, thresh2, 3?????)
+    Canny(edges, edges, 5, 70, 3);
+    ***************************
+    */
+    imwrite("edges.png", edges);
+    imwrite("capture.png", frame);
+    imwrite("hsv.png", hsv);
+    imwrite("circlesOut.png", circlesOut);
+    return 0;
+}
